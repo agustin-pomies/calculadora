35e10733ffc0644aa9fbea2735d5768f5222a05e
Agustin <apomies28@gmail.com>
2017-07-05 22:08:21 -0300

    Fixed Down in Parenthesis

--- include/discreta.hpp
+++ include/discreta.hpp
@@ -10,10 +10,12 @@
     - phi(n)
 */
 
-type division_entera(type a; type b)
-    return (type)(a/b); 
+type division_entera(type a; type b) {return (type)(a/b)} 
 
-type modulo(type a; type b)
-    return a%b;
+type modulo(type a; type b) {return a%b}
+
+void desc_factorial(type a) {
+
+}
 
 #endif
