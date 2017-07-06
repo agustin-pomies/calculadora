333951afd8cb1142127507df16b9aae5c5338410
Agustin <apomies28@gmail.com>
2017-07-05 21:52:17 -0300

    Agregado main.cpp

---
+++ cpp/main.cpp
@@ -0,0 +1,45 @@
+/* CALCULADORA */
+#include "../include/polinomio.hpp"
+#include "../include/aritmetica.hpp"
+#include "../include/discreta.hpp"
+
+/* EASTER EGGS
+Dividir entre 0 - The Final Countdown / Europe
+Raiz cuadrada de negativo - Imagine / John Lennon
+300 - THIS IS SPARTA
+99 - 99 red balloons / Nena
+69 - summer of 69 / Brian Adams
+123 - abc 123 / Jackson 5
+0.07 - James Bond Theme
+
+FUNCIONALIDADES
+& Aritmetica Básica
+    (+, -, ×, ÷)
+& Constantes
+    - Pi
+    - e
+& Mat. Discreta
+    - Algoritmo de Euclides
+    - MCD y mcm
+    - División entera y módulo
+    - Descomposición factorial
+    - Coef. de Bezout
+    - phi(n)
+& Álgebra Lineal
+    - Matrices y determinantes
+& Cálculo
+    - Integración y derivación
+& Funciones
+    - Ruffini
+
+NÚMEROS DESTACADOS
+007, 69, 42, 99, 123, 300, 666, 
+
+MODOS
+42 - todas las respuestas son 42
+*/
+
+int main() {
+
+    return 0;    
+}
