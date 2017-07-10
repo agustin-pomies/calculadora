c78b7d3304865658581607a3b3098745323508b6
Agustin <apomies28@gmail.com>
2017-07-10 11:51:48 -0300

    Added Cola

---
+++ cpp/cola.cpp
@@ -0,0 +1,52 @@
+#include <stddef.h>
+
+struct rep_nodo {
+	vertice dato;
+	rep_nodo* sig;
+};
+
+struct rep_cola {
+	rep_nodo* ppio;
+	rep_nodo* fin;
+};
+
+cola crear_cola() {
+	cola nuevo = new rep_cola;
+	nuevo->ppio = nuevo->fin = NULL;
+	return nuevo;
+}
+
+void encolar(vertice b, cola &c) {
+	rep_nodo* elem = new rep_nodo;
+	elem->dato = b;
+	elem->sig = NULL;
+	if(es_vacia_cola(c))
+		c->ppio = elem;
+	else
+		c->fin->sig = elem;
+	c->fin = elem;
+}
+
+void desencolar(cola &c) {
+	if(!es_vacia_cola(c)) {
+		rep_nodo* aux = c->ppio;
+		if(c->ppio->sig == NULL)
+			c->fin = NULL;
+		c->ppio = c->ppio->sig;
+		delete(aux);
+	};
+}
+
+bool es_vacia_cola(cola c) {
+	return c->ppio == NULL;
+}
+
+vertice frente(cola c) {
+	return c->ppio->dato;
+}
+
+void liberar_cola(cola &c) {
+	while(!es_vacia_cola(c))
+		desencolar(c);
+	delete(c);
+}
---
+++ include/cola.hpp
@@ -0,0 +1,40 @@
+#ifndef _COLA_H
+#define _COLA_H
+
+struct rep_cola;
+typedef rep_cola *cola;
+
+/* Constructoras */
+
+/*  Devuelve una cola_binarios vacía (sin elementos). */
+cola crear_cola_binarios();
+
+/* Encola `b' en `c'. */
+void encolar(vertice a, cola &c);
+
+/* Destructoras */
+
+/*
+  Remueve de `c' el elemento que está en el frente.
+  No libera la memoria del elemento removido.
+  Si es_vacia_cola(c) no hace nada.
+ */
+void desencolar(cola &c);
+
+/* Libera la memoria asignada a `c', pero NO la de sus elementos. */
+void liberar_cola(cola &c);
+
+/* Predicados */
+
+/* Devuelve `true' si y sólo si `c' es vacía (no tiene elementos). */
+bool es_vacia_cola(cola c);
+
+/* Selectoras */
+
+/*
+  Devuelve el elemento que está en el frente de `c'.
+  Precondición: ! es_vacia_cola(c);
+ */
+vertice frente(cola c);
+
+#endif
